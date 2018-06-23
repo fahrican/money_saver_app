5ac41ea7e8dbbb6583a2b0b42aab639dfd224b68
Fahri MacBook <fahri-can@web.de>
2018-06-23 19:25:25 +0200

    Start with development project, add two menu items: food and drinks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { FoodPage } from "../pages/food/food";
+import {DrinksPage} from "../pages/drinks/drinks";
 
 @Component({
   templateUrl: 'app.html'
@@ -21,8 +22,9 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'home', component: HomePage },
+      { title: 'food', component: FoodPage },
+      { title: 'drinks', component: DrinksPage },
     ];
 
   }
