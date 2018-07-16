e0273dd2ae75ef032177811632b6448a21ad0a27
Fahri MacBook <fahri-can@web.de>
2018-07-17 00:08:32 +0200

    Add ion select for radio buttons.

--- src/pages/add-loan/add-loan.ts
+++ src/pages/add-loan/add-loan.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component, ViewChild} from '@angular/core';
+import { IonicPage, NavController, AlertController, NavParams } from 'ionic-angular';
+import {StorageKeys} from "../../app/app.component";
+import {HomePage} from "../home/home";
+import {Storage} from '@ionic/storage';
+import {RadioButton} from "ionic-angular/umd";
+
 
 /**
  * Generated class for the AddLoanPage page.
@@ -15,11 +20,39 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddLoanPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  @ViewChild('loanDate') foodDate;
+  private lDate = "";
+
+  @ViewChild('loanAmount') foodAmount;
+  private lAmount: number = 0;
+
+  @ViewChild('loanNameOfFriend') foodPaymentMethod;
+  private lNameOfFriend = "";
+
+  @ViewChild('loanNote') foodNote;
+  private lNote = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+              private alertCtrl: AlertController, private storage: Storage) {
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AddLoanPage');
+  }
+
+  presentAlert() {
+
+    let alert = this.alertCtrl.create({
+      title: 'Field is empty!',
+      subTitle: 'Every field needs a input!!!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
+  mcqAnswer(value){
+    console.log(value);
   }
 
 }
